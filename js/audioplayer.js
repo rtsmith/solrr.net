dff8793e4bb2c31d2ca66bb4aec53fa718194923
Thomas Smith <tom.smith@imulus.com>
2014-06-07 17:23:30 -0600

    Add soundmanger for streaming

--- js/audioplayer.js
+++ js/audioplayer.js
@@ -3,6 +3,7 @@ var client_id = "ceed35cb3c4ba8b5d498475ae6e966b6";
 function Audioplayer(sc_url) {
     this.url = sc_url;
     this.endpoint = "http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/" + this.url + "&client_id=" + client_id;
+    this.resolve();
 }
 Audioplayer.prototype.resolve_callback = function(data) {
     this.track_id = data.id;
@@ -11,9 +12,24 @@ Audioplayer.prototype.resolve_callback = function(data) {
 }
 Audioplayer.prototype.resolve = function() {
     var that = this;
-    var result = $.get( this.endpoint, function(data ) { that.resolve_callback(data) } ); 
+    $.get( this.endpoint, function(data) { that.resolve_callback(data) } ); 
 }
 
 var track1 = new Audioplayer("mezla/mini-fuge");
 
-track1.resolve();
+
+// Sound manager /////////////////////////////////////////////////////////////////////
+var streamer = {
+    current_track : "/tracks/293",
+    play : function() {
+        SC.stream(this.current_track, function(sound) {
+            sound.play();
+        });
+    },
+    stop : function() {
+        soundManager.stopAll();
+    }
+}
+
+
+
