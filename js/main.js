8a8d49c727a93d95305538a029e3eae443eb9390
Thomas Smith <tom.smith@imulus.com>
2014-06-07 22:43:46 -0600

    Initial style for audio player

--- js/main.js
+++ js/main.js
@@ -9,15 +9,16 @@ $(function() {
         generateControls($player, track);
     }
     function generateControls($element, track) {
-        var $play_button = $('<a class="btn">Play1</a>')
+        var $play_button = $('<a class="btn play"><span class="glyphicon glyphicon-play"></span></a>')
             .on('click', function() {
                 streamer.play(track);
             });
-        var $pause_button = $('<a class="btn">Pause1</a>')
+        var $pause_button = $('<a class="btn pause"><span class="glyphicon glyphicon-pause"></span></a>')
             .on('click', function() {
                 streamer.pause();
             });
         var $controls = $('<div class="controls"></div>').append($play_button).append($pause_button);
-        $element.append($controls);
+        var meta = "<h4>Track Title - Artist</h4>";
+        $element.append($controls).append(meta);
     }
 });
