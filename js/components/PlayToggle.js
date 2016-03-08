a2ed9ceb1a9b2bb761f70a733889578028d91d3e
Tom Smith <tom@solrr.net>
2016-03-08 11:45:37 -0700

    Add Fontawesome and conditional playToggle class

--- js/components/PlayToggle.js
+++ js/components/PlayToggle.js
@@ -1,12 +1,13 @@
 var React = require('react');
 
 const PlayToggle = React.createClass({
-
   render: function() {
-    const trackPlaying = this.props.track_data.idLoaded == this.props.id && this.props.track_data.isPlaying ? "playing" : "stopped";
+    const trackClass = this.props.track_data.idLoaded == this.props.id && this.props.track_data.isPlaying ? "fa fa-pause" : "fa fa-play";
     return (
-      <div className={trackPlaying}>
-        <a onClick={this.props.onToggleClick}>play button</a>
+      <div className="play-toggle">
+        <a onClick={this.props.onToggleClick}>
+          <i className={trackClass}></i>
+        </a>
       </div>
     )
   }
