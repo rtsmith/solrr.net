543a19eef9f92f71383a3ee4e71c5f3c1cc0cefd
Tom Smith <tom@solrr.net>
2016-04-20 09:03:45 -0600

    Remake low level components as stateless fxns

--- js/components/PlayToggle.js
+++ js/components/PlayToggle.js
@@ -1,16 +1,15 @@
-var React = require('react');
+var React = require('react')
 
-const PlayToggle = React.createClass({
-  render: function() {
-    const trackClass = this.props.state_data.trackStatus == "playing" && this.props.state_data.data.id == this.props.state_data.idLoaded
-      ? "fa fa-pause" : "fa fa-play";
-    return (
-      <div className="play-toggle">
-        <a onClick={this.props.onToggleClick}>
-          <i className={trackClass}></i>
-        </a>
-      </div>
-    )
-  }
-});
+var PlayToggle = (props) => {
+  var trackClass = props.state_data.trackStatus === "playing" && props.state_data.data.id === props.state_data.idLoaded
+    ? "fa fa-pause" : "fa fa-play";
+  return (
+    <div className="play-toggle">
+      <a onClick={props.onToggleClick}>
+        <i className={trackClass}></i>
+      </a>
+    </div>
+  )
+};
+
 module.exports = PlayToggle;
--- js/components/SeekBar.js
+++ js/components/SeekBar.js
@@ -1,30 +1,28 @@
 var React = require('react')
 var Time = require('../helpers/time');
 
-var SeekBar = React.createClass({
+var SeekBar = (props) => {
+  var pos = Time.format(props.seek) || '';
+  var relativePos = `${(props.seek / props.duration) * 100 || 0}%`;
+  var style = {
+    backgroundImage: `url(${props.wave_url})`,
+    backgroundSize: "cover"
+  };
+  var posStyle = {
+    display: props.seek > 0 ? "inline" : "none"
+  };
+  var seekStyle = {
+    width: relativePos,
+    display: props.seek > 0 ? "block" : "none"
+  };
 
-  render: function() {
-    var pos = Time.format(this.props.seek) || '';
-    var relativePos = `${(this.props.seek / this.props.duration) * 100 || 0}%`;
-    var style = {
-      backgroundImage: `url(${this.props.wave_url})`,
-      backgroundSize: "cover"
-    };
-    var posStyle = {
-      display: this.props.seek > 0 ? "inline" : "none"
-    };
-    var seekStyle = {
-      width: relativePos,
-      display: this.props.seek > 0 ? "block" : "none"
-    };
-    return (
-      <div onClick={this.props.listenSeek} className="seekbar" style={style}>
-        <div className="seek-pos" style={seekStyle}></div>
-        <span style={posStyle}>{pos}</span> 
-        <span>{Time.format(this.props.duration)}</span>
-      </div>
-    )
-  }
-});
+  return (
+    <div onClick={props.listenSeek} className="seekbar" style={style}>
+      <div className="seek-pos" style={seekStyle}></div>
+      <span style={posStyle}>{pos}</span>
+      <span>{Time.format(props.duration)}</span>
+    </div>
+  )
+};
 
 module.exports = SeekBar;
